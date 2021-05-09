74dfba22c049fc78dafb4d4596b1be3547da3cf5
AKSHAY KR <ashayakr4@gmail.com>
2021-05-09 14:47:02 +0530

    event binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,10 +9,52 @@ export class LoginComponent implements OnInit {
 
   aim="Your Perfect Bankimg Partner"
   account="Account Number Please"
+  accno=""
+  pswd=""
+ 
+  AccountDetails:any = {
+    1000: { acno: 1000, pswd: "user1", blnce: 5000 , uname:"user1"},
+    1001: { acno: 1001, pswd: "user2", blnce: 3000 , uname:"user2"},
+    1002: { acno: 1002, pswd: "user3", blnce: 10000 , uname:"user3"},
+    1003: { acno: 1003, pswd: "user4", blnce: 1000 , uname:"user4"},
+}
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ChangeAcc(event:any){
+
+    this.accno = event.target.value
+    console.log(this.accno);
+    
+  }
+  ChangePswd(event:any){
+
+    this.pswd = event.target.value
+    console.log(this.pswd);
+    
+
+  }
+
+  login(){
+    
+    var acno=this.accno
+    var pswd= this.pswd
+
+    let dataset = this.AccountDetails
+    if (acno in dataset) {
+      if (pswd == dataset[acno]["pswd"]) {
+        alert("login Succesfull")
+      }
+      else {
+        alert("Invalid Password")
+      }
+
+    }
+    else {
+      alert("Invalid Account Number, Please Register")
+    }
+  }
 }
